81db6b73ff525dad6c9dcedd31fe90d279e90e70
hectordelunaa <107322480+hectordelunaa@users.noreply.github.com>
2022-10-19 22:52:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Funny Verbs
+## A way to learn english verbs with a JS App
+
+### == I'm still working on it, adding verbs ==
+
+I had the idea of make a *funny way* to learn verbs, since my children was trying to _learn verbs_ in english because the high-school.
+
+I was inspired in Apps like __Duolingo__ and sites like __freerice.com__ to create something that could help my sons.
+
+First thing first, was the programming language, so I decided the simplest way and what I knew, so then I chose __javascript__ in a web environment only in a front-end way.
+
+I've added more verbs!
---
+++ verbs.js
@@ -0,0 +1,122 @@
+
+
+const verbs = [
+  'open',
+  'love',
+  
+  'dance',
+  
+  'walk',
+  'close',
+  'cook',
+  'count',
+  'wish',
+  'start',
+
+  'listen',
+  'expect',
+  'wait',
+  'study',
+  
+  'smoke',
+  'talk',
+  
+  'play',
+  'wash',
+  'clean',
+  'complete',
+  'check',
+
+  'share',
+
+  'look',
+  'show',
+  
+  'name',
+  
+  'stop',
+  'stay',
+  
+  'ask',
+  'want',
+  'remember',
+  'rent',
+  'repeat',
+  
+  'jump',
+  'follow',
+
+  'work',
+  'climb',
+  'use',
+  
+  'travel',
+  'visit',
+  'live',
+
+  'act',
+  'watch',
+  
+  'read'
+];
+
+const verbos = [
+  'abrir',
+  'amar',
+  
+  'bailar',
+  
+  'caminar',
+  'cerrar',
+  'cocinar',
+  'contar',
+  'desear',
+  'comenzar',
+  
+  'escuchar',
+  'suponer',
+  'esperar',
+  'estudiar',
+  
+  'fumar',
+  'hablar',
+  
+  'jugar',
+  'lavar',
+  'limpiar',
+  'completar',
+  'checar',
+  
+  'compartir',
+  
+  'mirar',
+  'mostrar',
+  
+  'nombrar',
+
+  'parar',
+  'quedarse',
+
+  'preguntar',
+  'querer',
+  'recordar',
+  'rentar',
+  'repetir',
+  
+  'saltar',
+  'seguir',
+  
+  'trabajar',
+  'trepar',
+  'usar',
+  
+  'viajar',
+  'visitar',
+  'vivir',
+  
+  'actuar',
+  'observar',
+  
+  
+  'leer'  
+];
